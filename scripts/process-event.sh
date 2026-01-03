451fc30ac69e72d85bc43adfae365f8e02efafa7
Brucie83 <bdiazbarba@gmail.com>
2026-01-03 13:18:20 -0600

    feat: add issue auto-comment using GitHub Actions

--- scripts/process-event.sh
+++ scripts/process-event.sh
@@ -8,7 +8,7 @@ EVENT_JSON="$EVENT_PATH"
 TITLE=$(jq -r '.issue.title' "$EVENT_JSON")
 AUTHOR=$(jq -r '.issue.user.login' "$EVENT_JSON")
 ISSUE_NUMBER=$(jq -r '.issue.number' "$EVENT_JSON")
-LABELS=$(jq -r '[.issue.labels[].name]' "$EVENT_JSON")
+LABELS=$(jq -c '[.issue.labels[].name]' "$EVENT_JSON")
 
 cat <<EOF > output/event.json
 {
@@ -21,4 +21,21 @@ cat <<EOF > output/event.json
 }
 EOF
 
-  echo "Event processed successfully"
+echo "Event processed successfully"
+
+COMMENT_BODY=$(jq -n \
+  --arg title "$TITLE" \
+  --arg author "$AUTHOR" \
+  --argjson labels "$LABELS" \
+  '{
+    body: ("Thanks for opening this issue!\n\n" +
+           "Title: " + $title + "\n" +
+           "Author: " + $author + "\n" +
+           "Labels: " + (if ($labels | length) == 0 then "none" else ($labels | join(", ")) end))
+   }')
+
+curl -sf -X POST \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "Accept: application/vnd.github+json" \
+  https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/$ISSUE_NUMBER/comments \
+  -d "$COMMENT_BODY"
